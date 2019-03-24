a107c7e26a0d8b36c86c03755b8bf8a71b1b9135
Anilmal <baranovart231999@gmail.com>
2019-03-24 14:45:15 +0300

    А вот и документики подъехали

--- base/Table.h
+++ base/Table.h
@@ -48,45 +48,24 @@ public:
 struct Pair
 {
 	int key;
-	TList<T> values;
+	TList<TPolinom> values;
 };
+
 int Hash(int key)
 {
-	return key % HASH_SIZE;
+	return key % MaxSize;
 }
 class HashTable
 {
-
- private:
+private:
 	Pair table[MaxSize];
- public:
-	 HashTable();
+public:
+	HashTable();
 	void Delete(string first_key, int key_)
 	{
 		int index = 0;
 		int tmp = Hash(key_);
-		Node *p = table[tmp].values->pFirst;
-		while (p != nullptr)
-		{
-			if (p->key != first_key)
-			{
-				p = p->pNext;
-				index++;
-			}
-			if (p->key == first_key)
-				table[tmp].values.DeleteEl(index);
-		}
-	}
-	void Add(T elem, int key_)
-	{
-		int tmp = Hash(key_);
-		table[tmp].values.push_back(elem);
-
-	}
-	TPolinom Get(ValType first_key, int key_)
-	{
-		int tmp = Hash(key_);
-		Node *p = table[tmp].values->pFirst;
+		Node<TPolinom> *p = table[tmp].values.begin();
 		while (p != nullptr)
 		{
 			if (p->key != first_key)
@@ -95,10 +74,73 @@ class HashTable
 				index++;
 			}
 			if (p-> == first_key)
+				table[tmp].values.DeleteEl(index);
+		}
+	}
+	void Add(TPolinom elem, int key_)
+	{
+		int tmp = Hash(key_);
+		table[tmp].values.push_back(elem);
+
+	}
+	TPolinom Get(string first_key, int key_)
+	{
+		int index = 0;
+		int tmp = Hash(key_);
+		Node<TPolinom> *p = table[tmp].values.begin();
+		while (p != nullptr)
+		{
+			if (p->key != first_key)
+			{
+				p = p->pNext;
+				index++;
+			}
+			if (p->key == first_key)
 				return table[tmp].values[index];
 		}
 	}
 };
+struct tab
+{
+	int key;
+	TPolinom values;
+	bool flag = false;
+};
+
+class HashTable
+{
+private:
+	tab table[HASH_SIZE];
+public:
+	HashTable() :
+		void Delete(int key_)
+	{
+		int index = 0;
+		int tmp = Hash(key_);
+
+	}
+	void Add(TPolinom elem, int key_)
+	{
+		int tmp = Hash(key_);
+		if (!table[tmp].flag)
+			table[tmp].values = elem;
+		else
+		{
+			while (table[tmp].flag == true)
+			{
+				tmp++;
+			}
+			table[tmp].values = elem;
+		}
+
+	}
+	TPolinom Get(int key_)
+	{
+		int tmp = Hash(key_);
+
+	}
+};
+
 
 
 
