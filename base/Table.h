97f6c4df5f3405cd0872b4d2279b7989f6121487
Баранов Артем Алексеевич <baranov.a.a@ccam.cmc.unn.net>
2019-03-26 16:12:09 +0300

    I am Batman

--- base/Table.h
+++ base/Table.h
@@ -3,48 +3,48 @@
 #include "list.h"
 const int MaxSize = 100;
 
-template <class T> struct TRecord
-{
-	TList<T> data;
-	int key;
-};
-
-template <class T> class TableLine
-{
-	Cell<T, string>data[MaxSize];
-public:
-	void Insert(T _data, string _key);
-	void Delete(string _key);
-	T Search(string _key);
-};
-
-template <class T> class TableSort
-{
-	Cell<T, string>data[MaxSize];
-public:
-	void Insert(T _data, string _key);
-	void Delete(string _key);
-	T Search(string _key);
-};
-
-template <class T> class TableList
-{
-	TList<Cell<T, string>> data;
-public:
-	TableList();
-	void Insert(T _data, string _key);
-	void Delete(string _key);
-	T Search(string _key);
-};
-
-template <class T> class TableTree
-{
-	//Cell<T, string>data[MaxSize];
-public:
-	void Insert(T _data, string _key);
-	void Delete(string _key);
-	T Search(string _key);
-};
+//template <class T> struct TRecord
+//{
+//	TList<T> data;
+//	int key;
+//};
+//
+//template <class T> class TableLine
+//{
+//	Cell <T, string>data[MaxSize];
+//public:
+//	void Insert(T _data, string _key);
+//	void Delete(string _key);
+//	T Search(string _key);
+//};
+//
+//template <class T> class TableSort
+//{
+//	Cell<T, string>data[MaxSize];
+//public:
+//	void Insert(T _data, string _key);
+//	void Delete(string _key);
+//	T Search(string _key);
+//};
+//
+//template <class T> class TableList
+//{
+//	TList<Cell<T, string>> data;
+//public:
+//	TableList();
+//	void Insert(T _data, string _key);
+//	void Delete(string _key);
+//	T Search(string _key);
+//};
+//
+//template <class T> class TableTree
+//{
+//	//Cell<T, string>data[MaxSize];
+//public:
+//	void Insert(T _data, string _key);
+//	void Delete(string _key);
+//	T Search(string _key);
+//};
 
 
 template<class T> struct Cell
@@ -96,6 +96,18 @@ public:
 				return table[tmp][i].data;
 		}
 	}
+	void Show()
+	{
+		for (int i = 0; i < MaxSize; i++)
+		{
+			cout << i << "|";
+			for (int j = 0; j < table[tmp].GetSize(); j++)
+			{
+				count << table[i][j].data << "|";
+			}
+			cout << endl;
+		}
+	}
 };
 template <class T> struct tab
 {
@@ -107,7 +119,7 @@ template <class T> class Hash_Table
 {
 private:
 	tab<T> table[MaxSize];
-	int count=0;
+	int count = 0;
 	tab<T> GetTab()
 	{
 		return table;
@@ -116,16 +128,16 @@ public:
 	Hash_Table() {};
 	void Delete(int key_)
 	{
-			tmp = Hash(key_);
-			while (table[tmp].key != key_)
-			{
-				tmp++;
-			}
-			if (table[tmp].key == key_)
-			{
-				table[tmp].flag = false;
-				count--;
-			}
+		tmp = Hash(key_);
+		while (table[tmp].key != key_)
+		{
+			tmp++;
+		}
+		if (table[tmp].key == key_)
+		{
+			table[tmp].flag = false;
+			count--;
+		}
 	}
 	void Insert(T elem, int key_)
 	{
@@ -144,25 +156,22 @@ public:
 	T Search(int key_)
 	{
 		tmp = Hash(key_);
-		while (table[tmp].key != key_)
+		while (table[tmp].flag)
 		{
+			if (table[tmp].key == key_)
+				return table[tmp].rec;
 			tmp++;
 		}
-		if (table[tmp].key == key_)
-			return table[tmp].rec;
-		else
-			throw"key is not found";
+		throw "Key is not found";
 	}
-	friend ostream& operator<<(ostream &out, const Hash_Table &t)
+	void Show()
 	{
-		int tmp = MaxSize;
-		for (int i = MaxSize; i > 0; i--)
+		for (int i = 0; i < MaxSize; i++)
 		{
-			i -= tmp;
-			out <<i <<"|"<<t.Search(i) << "|";
-			tmp--;
+			cout << i<<"|";
+			if (table[i].flag)
+				count << table[i].rec << "|" << endl;
 		}
-		return out;
 	}
 };
 
